8bf93197cb0707e29ebbd71d1658162b97b302d5
Dali07 Heam <dali75943@gmail.com>
2025-11-23 13:41:58 +0000

    karya seni ganti hasil project saya aja bro

--- src/components/sections/projects.tsx
+++ src/components/sections/projects.tsx
@@ -7,7 +7,7 @@ export function Projects() {
       <div className="container mx-auto px-4 md:px-6">
         <div className="flex flex-col items-center justify-center space-y-4 text-center">
           <div className="space-y-2">
-            <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl font-headline">Karya Saya</h2>
+            <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl font-headline">Hasil Project Saya</h2>
             <p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
               Berikut adalah beberapa proyek yang saya banggakan. Setiap proyek mengajarkan saya sesuatu yang baru dan membantu saya berkembang.
             </p>
